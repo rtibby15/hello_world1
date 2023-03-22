2ab72c265d4227fe3e1e15dbedd33e0f4855ad07
rtibby15 <128619135+rtibby15@users.noreply.github.com>
2023-03-22 12:44:41 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,10 +5,10 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
+       String firstName = "Rakesh Tibby";
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
-
+	System.out.println("Hello, I am ", firstname);
     }
 }
 
